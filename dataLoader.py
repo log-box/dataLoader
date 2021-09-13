ef9e6af0bcfe614256df52b44d673d56c71352cf
log-box <a.y.logvin@gmail.com>
2021-09-13 13:53:51 +0300

    on work...

--- dataLoader.py
+++ dataLoader.py
@@ -6,15 +6,55 @@ import psycopg2
 from psycopg2 import Error
 
 USER = 'postgres'
-# USER = 'postgress' https://pythonru.com/biblioteki/operacii-insert-update-delete-v-postgresql  сравнивать значения с 2 по 13
 PASSWORD = '1'
 DATABASE = 'dataloader'
+DATABASE_TABLE = "dataLoader_questions"
 PORT = '5432'
 HOST = '127.0.0.1'
 CSV_PATH = 'data.csv'
+connection = ''
+clear_database_records_count = 1
+new_records_count = 0
+reader = ''
+count = 0
+# https://pythonru.com/biblioteki/operacii-insert-update-delete-v-postgresql
+
+def row_recording(string, connection, cursor):
+    #  Recording new row in DATABASE_TABLE
+    global clear_database_records_count
+    id = string[0]
+    question = string[1]
+    rightAnswer = string[3]
+    commentForJudge = string[4]
+    aboutQuestion = string[5]
+    link = string[6]
+    wrongAnswerOne = string[7]
+    wrongAnswerTwo = string[8]
+    wrongAnswerThree = string[9]
+    themeOfQuestion = string[10]
+    questionCategory = string[11]
+    sectionOfQuestion = string[12]
+    complexityOfQuestion = string[14]
+    user = string[16]
+    approve = 1
+    date_ques_sub = string[17]
+    base_date = string[20]
+    sql_query = 'INSERT INTO "dataLoader_questions" (id, question,"rightAnswer","commentForJudge","aboutQuestion",' \
+                'link,"wrongAnswerOne","wrongAnswerTwo","wrongAnswerThree","themeOfQuestion","questionCategory"' \
+                ',"sectionOfQuestion","complexityOfQuestion","user",approve,date_ques_sub,base_date) ' \
+                'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+    data = (
+        id, question, rightAnswer, commentForJudge, aboutQuestion, link, wrongAnswerOne,
+        wrongAnswerTwo,
+        wrongAnswerThree, themeOfQuestion, questionCategory, sectionOfQuestion,
+        complexityOfQuestion, user, approve,
+        date_ques_sub, base_date)
+    cursor.execute(sql_query, data)
+    connection.commit()
 
 
 def clear_db():
+    #  Completely clearing DATABASE_TABLE by CASCADE method
     connection = ''
     try:
         connection = psycopg2.connect(
@@ -25,14 +65,14 @@ def clear_db():
             port=PORT
         )
         cursor = connection.cursor()
-        print('WARNING!!!\nThis operations will DELET ALL DATA IN DATABASE!\n')
+        print('!!! WARNING !!!\nTHIS OPERATION WILL DELET ALL DATA IN DATABASE!\n')
         sleep(5)
         userInput = input('Are you sure?[y/n]\n')
         if userInput.lower() == 'y':
             postgreDeleteAll = 'TRUNCATE "dataLoader_questions" RESTART IDENTITY CASCADE;'
             cursor.execute(postgreDeleteAll)
             connection.commit()
-            print(f'Database "{DATABASE}" was cleaned !!!')
+            print(f'!!! Database "{DATABASE}" was cleaned !!!')
         else:
             print('Operation was canceled!!! DATA was NOT deleted!')
 
@@ -47,6 +87,7 @@ def clear_db():
 
 
 def read_csv_file():
+    #  Reading CSV file and return list of lists and list of dicts
     csv_path = CSV_PATH
     try:
         with open(csv_path, "r", encoding='utf-8', newline='') as f_obj:
@@ -87,9 +128,11 @@ def read_csv_file():
 
 
 def main():
-    connection = ''
-    i = 1
-    reader = ''
+    global connection
+    global clear_database_records_count
+    global new_records_count
+    global reader
+    global count
     try:
         connection = psycopg2.connect(
             database=DATABASE,
@@ -104,74 +147,68 @@ def main():
             print(f'Error in parsing file [{CSV_PATH}]:\n first cell value must be [#]\n '
                   f'first string must be without any data')
         else:
-            postgreSelectAll = 'select * from "dataLoader_questions"'
+            postgreSelectAll = 'select * from "dataLoader_questions" order by "id"'
             cursor.execute(postgreSelectAll)
             sqlData = cursor.fetchall()
-            print(f'Records in database: {len(sqlData)}')
             if len(sqlData) == 0:
+                #  Database is empty, doing filing
                 print('Database is empty. Loading DATA into it')
                 sleep(3)
                 for string in reader:
                     if string[0] != '#':
-                        id = string[0]
-                        question = string[1]
-                        rightAnswer = string[3]
-                        commentForJudge = string[4]
-                        aboutQuestion = string[5]
-                        link = string[6]
-                        wrongAnswerOne = string[7]
-                        wrongAnswerTwo = string[8]
-                        wrongAnswerThree = string[9]
-                        themeOfQuestion = string[10]
-                        questionCategory = string[11]
-                        sectionOfQuestion = string[12]
-                        complexityOfQuestion = string[14]
-                        user = string[16]
-                        approve = 1
-                        date_ques_sub = string[17]
-                        base_date = string[20]
-                        sqlData = 'INSERT INTO "dataLoader_questions" (id, question,"rightAnswer","commentForJudge","aboutQuestion",' \
-                                  'link,"wrongAnswerOne","wrongAnswerTwo","wrongAnswerThree","themeOfQuestion","questionCategory"' \
-                                  ',"sectionOfQuestion","complexityOfQuestion","user",approve,date_ques_sub,base_date) ' \
-                                  'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
-                        data = (
-                            id, question, rightAnswer, commentForJudge, aboutQuestion, link, wrongAnswerOne,
-                            wrongAnswerTwo,
-                            wrongAnswerThree, themeOfQuestion, questionCategory, sectionOfQuestion,
-                            complexityOfQuestion, user, approve,
-                            date_ques_sub, base_date)
-                        cursor.execute(sqlData, data)
-                        print(f'Строка№ {i} записана успешно')
-                        i += 1
-                        connection.commit()
+                        row_recording(string,connection,cursor)
+                        clear_database_records_count += 1
+                print(f'Total row{"s" if clear_database_records_count > 1 else ""} {clear_database_records_count} successfully added')
             else:
+                #  Data base not empty
+                print(f'Records in database: {len(sqlData)}')
                 fields = (
                     "id", "user", "question", "linkOfPicture", "rightAnswer", "wrongAnswerOne", "wrongAnswerTwo",
                     "wrongAnswerThree", "commentForJudge", "aboutQuestion", "link"
                     , "themeOfQuestion", "questionCategory", "sectionOfQuestion", "complexityOfQuestion",
                     "approve", "date_ques_sub", "base_date")
-                for item in range(len(sqlData) - 1):
-                    if sqlData[item][0] == int(reader[item][0]):
-                        for i in range(18):
-                            if sqlData[item][i] != reader_dicts[item][fields[i]]:
-                                sql_update_query = f'Update "dataLoader_questions" set {fields[i]} = %s where {fields[0]} = %s'
-                                cursor.execute(sql_update_query, (reader_dicts[item][fields[i]], reader_dicts[item][fields[0]]))
-                                connection.commit()
-                                count = cursor.rowcount
-                                print("Запись успешно обновлена")
-                                # print(f'ELM: #{item} data mismatch in {sqlData[item][i]}\n{reader_dicts[item][fields[i]]}')
-                print(f'Total records update: {count}')
+                for item in range(len(reader)):
+                    try:
+                        if sqlData[item][0] == int(reader[item][0]):
+                            #  Checking available rows for changes
+                            updated = False
+                            updated_fields = []
+                            for index in range(18):
+                                #  Checking data in columns and UPDATE if mismatch
+                                if sqlData[item][index] != reader_dicts[item][fields[index]]:
+                                    sql_update_query = f'Update "dataLoader_questions" set "{fields[index]}" = %s where {fields[0]} = %s'
+                                    cursor.execute(sql_update_query,
+                                                   (reader_dicts[item][fields[index]], reader_dicts[item][fields[0]]))
+                                    connection.commit()
+                                    updated = True
+                                    updated_fields.append(fields[index])
+                            if updated:
+                                print(
+                                    f'Record with ID "{reader_dicts[item][fields[0]]}" updated in "{updated_fields}" field{"s" if len(updated_fields) > 1 else ""}.')
+                                count += 1
+                    except IndexError:
+                        #  If rows in CSV file more than in DB. Adding new rows
+                        row_recording(reader[item], connection, cursor)
+                        new_records_count += 1
+                        print(f'New data with ID "{reader[item][0]}" successfully added')
+                if count > 0:
+                    print(f'Total records updated: {count}')
+                else:
+                    print('There no any changes in current questions. Data not updated.')
+                if new_records_count > 0:
+                    print(f'Added {new_records_count} new record{"s" if new_records_count > 1 else ""}')
     except KeyError:
         print('Error in indexes\nCall developer +79200631679')
     except (Exception, Error) as error:
-        if i < len(reader):
-            print(f'Loading is not completed\nTotal records to load: [{len(reader)}]\nLoaded [{i - 1}] records')
+        if clear_database_records_count < len(reader):
+            print(
+                f'Loading is not completed\nTotal records to load: [{len(reader)}]\nLoaded [{clear_database_records_count - 1}] records')
         print('Database or connection error\n', error)
     finally:
         if connection:
             cursor.close()
             connection.close()
-            print('PostgreSQL connection closed')
+            print('PostgreSQL connection closed.')
 
 
 if __name__ == "__main__":
